7cd44a590ef7659409e8789b36e0ec9394b5ed63
Marius B. Kotsbak <marius@kotsbak.com>
2015-09-10 10:38:53 +0200

    Add implicit logical operators for ReadChannel[Boolean]
    Operators &&, || and !, as well as onTrue and onFalse/isFalse transformations
    
    Closes #9

--- shared/src/main/scala/pl/metastack/metarx.scala
+++ shared/src/main/scala/pl/metastack/metarx.scala
@@ -12,4 +12,31 @@ package object metarx extends OptImplicits {
       opt := Some(t)
     }
   }
+
+  implicit class ReadChannelBooleanExtensions(rch: ReadChannel[Boolean]) {
+    def &&(other: ReadChannel[Boolean]): ReadChannel[Boolean] =  {
+      rch.flatMap (thisVal => other.map(otherVal => thisVal && otherVal) )
+    }
+    def &&(argument: Boolean): ReadChannel[Boolean] = {
+      rch.map(value => value && argument)
+    }
+
+    def ||(other: ReadChannel[Boolean]): ReadChannel[Boolean] =  {
+      rch.flatMap (thisVal => other.map(otherVal => thisVal || otherVal) )
+    }
+    def ||(argument: Boolean): ReadChannel[Boolean] = {
+      rch.map(value => value || argument)
+    }
+
+    def isFalse: ReadChannel[Boolean] = rch.map(!_)
+    def unary_! = isFalse
+
+    def onTrue: ReadChannel[Unit] = rch.collect { case true => Unit }
+    def onFalse: ReadChannel[Unit] = rch.collect { case false => Unit }
+  }
+
+  implicit class BooleanExtensions(boolVal: Boolean) {
+    def &&(rch: ReadChannel[Boolean]): ReadChannel[Boolean] = rch && boolVal
+    def ||(rch: ReadChannel[Boolean]): ReadChannel[Boolean] = rch || boolVal
+  }
 }
