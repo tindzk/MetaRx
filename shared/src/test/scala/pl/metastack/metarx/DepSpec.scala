8697b4d5d3941df31805383007dc5128a623f360
Matt Hicks <matt@outr.com>
2016-01-04 13:00:21 -0600

    Dep: Spec
    Minor additions to DepSpec.

--- shared/src/test/scala/pl/metastack/metarx/DepSpec.scala
+++ shared/src/test/scala/pl/metastack/metarx/DepSpec.scala
@@ -13,18 +13,26 @@ class DepSpec extends WordSpec with Matchers {
       "be initialized to zero" in {
         right.get should be(0.0)
       }
-
       "update when the width changes" in {
         width := 50.0
         x.get should be(0.0)
         width.get should be(50.0)
         right.get should be(50.0)
       }
-
       "update when x changes" in {
         x := 25.0
         right.get should be(75.0)
         width.get should be(50.0)
+      }
+      "update when right changes" in {
+        right := 100.0
+        x.get should be(50.0)
+        width.get should be(50.0)
+      }
+      "update right properly when width changes" in {
+        width := 25.0
+        right.get should be(100.0)
+        x.get should be(75.0)
       }
     }
   }
