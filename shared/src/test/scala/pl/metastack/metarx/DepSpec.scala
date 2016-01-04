025fd5950b92a7d4ada96d57dd94001029551ff7
Matt Hicks <matt@outr.com>
2016-01-04 11:30:07 -0600

    Dep: Spec
    Created preliminary DepSpec. Currently failing on change of dependent value.

---
+++ shared/src/test/scala/pl/metastack/metarx/DepSpec.scala
@@ -0,0 +1,28 @@
+package pl.metastack.metarx
+
+import org.scalatest.{Matchers, WordSpec}
+
+class DepSpec extends WordSpec with Matchers {
+  "Dep" when {
+    val x: Sub[Double] = Sub(0.0)
+    val width: Sub[Double] = Sub(0.0)
+
+    "creating simple dependencies" should {
+      val right = new Dep[Double](x, rc => rc - width, x.get + width.get)
+
+      "be initialized to zero" in {
+        right.get should be(0.0)
+      }
+      "update when the width changes" in {
+        width := 50.0
+        right.get should be(50.0)
+        x.get should be(0.0)
+      }
+      "update when x changes" in {
+        x := 25.0
+        right.get should be(75.0)
+        width.get should be(50.0)
+      }
+    }
+  }
+}
