924af569cd3a66987d8b706cfc1525d71cdc9504
Matt Hicks <matt@outr.com>
2016-01-05 08:06:08 -0600

    Dep: Spec
    Updated DepSpec to outline problem.

--- shared/src/test/scala/pl/metastack/metarx/DepSpec.scala
+++ shared/src/test/scala/pl/metastack/metarx/DepSpec.scala
@@ -35,5 +35,32 @@ class DepSpec extends WordSpec with Matchers {
         x.get should be(75.0)
       }
     }
+    "creating more complex dependencies" should {
+      val center = x.dep[Double](_ + (width.get / 2.0), _ - (width / 2.0))
+
+      val screenX = Sub(0.0)
+      val screenWidth = Sub(0.0)
+      val screenCenter = screenX.dep[Double](_ + (screenWidth.get / 2.0), _ - (screenWidth / 2.0))
+
+      "be initialized to the proper value" in {
+        center.get should be(87.5)
+        x.get should be(75.0)
+        screenX.get should be(0.0)
+        screenWidth.get should be(0.0)
+        screenCenter.get should be(0.0)
+      }
+      "set center to screen center" in {
+        center := screenCenter
+        x.get should be(-12.5)
+        center.get should be(0.0)
+      }
+      "update screen information" in {
+        screenX := 10.0
+        screenWidth := 500.0
+        screenCenter.get should be(260.0)
+        x.get should be(247.5)
+        center.get should be(260.0)
+      }
+    }
   }
 }
