40bc36325aa8be134e15780ac8c8c300be662541
Tim Nieradzik <tim@metastack.pl>
2016-01-05 16:42:16 +0100

    Dep: Propagate values

--- shared/src/main/scala/pl/metastack/metarx/Dep.scala
+++ shared/src/main/scala/pl/metastack/metarx/Dep.scala
@@ -4,7 +4,15 @@ class Dep[T, U](sub: Sub[T],
                 fwd: T => U,
                 bwd: U => ReadChannel[T]
                ) extends StateChannel[U] with ChannelDefaultSize[U] {
-  silentAttach(value => sub := bwd(value))
+  var skip = false
+
+  val ignore = silentAttach { value =>
+    skip = true
+    sub := bwd(value)
+    skip = false
+  }
+
+  sub.attach(value => if (!skip) produce(fwd(value), ignore))
 
   def produce(subscriber: ReadChannel[U]): Unit =
     sub := subscriber.flatMap(bwd)
--- shared/src/test/scala/pl/metastack/metarx/DepSpec.scala
+++ shared/src/test/scala/pl/metastack/metarx/DepSpec.scala
@@ -2,6 +2,8 @@ package pl.metastack.metarx
 
 import org.scalatest.{Matchers, WordSpec}
 
+import scala.collection.mutable.ArrayBuffer
+
 class DepSpec extends WordSpec with Matchers {
   "Dep" when {
     val x: Sub[Double] = Sub(0.0)
@@ -9,6 +11,9 @@ class DepSpec extends WordSpec with Matchers {
 
     "creating simple dependencies" should {
       val right = x.dep[Double](_ + width.get, _ - width)
+
+      val values = ArrayBuffer.empty[Double]
+      right.attach(values += _)
 
       "be initialized to zero" in {
         right.get should be(0.0)
@@ -28,6 +33,8 @@ class DepSpec extends WordSpec with Matchers {
         right := 100.0
         x.get should be(50.0)  // right - width
         width.get should be(50.0)
+
+        values should be (Seq(0.0, 75.0, 100.0))
       }
       "update `x` and `right` when `width` changes" in {
         width := 25.0
@@ -41,6 +48,9 @@ class DepSpec extends WordSpec with Matchers {
       val screenX = Sub(0.0)
       val screenWidth = Sub(0.0)
       val screenCenter = screenX.dep[Double](_ + (screenWidth.get / 2.0), _ - (screenWidth / 2.0))
+
+      val values = ArrayBuffer.empty[Double]
+      screenCenter.attach(values += _)
 
       "be initialized to the proper value" in {
         center.get should be(87.5)
@@ -56,9 +66,13 @@ class DepSpec extends WordSpec with Matchers {
       }
       "update screen information" in {
         screenX := 10.0
+        screenCenter.get should be(10.0)
         screenWidth := 500.0
         screenCenter.get should be(260.0)
-        x.get should be(247.5)
+
+        values should be (Seq(0.0, 10.0, 260.0))
+
+        x.get should be(247.5)  // screenCenter - (width / 2.0)
         center.get should be(260.0)
       }
     }
