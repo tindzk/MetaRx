c603775ad76f41a785be1c941c6ad1e56ada9933
Matt Hicks <matt@outr.com>
2016-01-06 10:41:14 -0600

    Dep: Functionality
    Removed unnecessary generation of multiple ReadChannels.

--- shared/src/main/scala/pl/metastack/metarx/Dep.scala
+++ shared/src/main/scala/pl/metastack/metarx/Dep.scala
@@ -4,11 +4,12 @@ package pl.metastack.metarx
 private[metarx] class Dep[T, U](sub: Sub[T],
                                 fwd: ReadChannel[T] => ReadChannel[U],
                                 bwd: ReadChannel[U] => ReadChannel[T]) {
-  private val internal = Sub[U](fwd(sub))
+  private val channel = fwd(sub)
+  private val internal = Sub[U](channel)
 
   def :=(channel: ReadChannel[U]): Unit = sub := bwd(channel)
   def :=(value: U): Unit = sub := bwd(Var(value))
   def get: U = internal.get
 
-  def toReadChannel: ReadChannel[U] = fwd(sub)
+  def toReadChannel: ReadChannel[U] = channel
 }
