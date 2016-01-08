02dd4f93152b1af6365b278ac9ea4a06e7a66bf5
Tim Nieradzik <tim@metastack.pl>
2016-01-08 15:50:02 +0100

    Dep: Get rid of `ignore`

--- shared/src/main/scala/pl/metastack/metarx/Dep.scala
+++ shared/src/main/scala/pl/metastack/metarx/Dep.scala
@@ -5,16 +5,12 @@ class Dep[T, U] private[metarx](sub: Sub[T],
                                 fwd: ReadChannel[T] => ReadChannel[U],
                                 bwd: ReadChannel[U] => ReadChannel[T])
   extends Sub[U](null.asInstanceOf[U]) {
-  private var ignore = false
-
   sub.attach { s =>
-    ignore = true
-    this := fwd(Var(s))
-    ignore = false
+    super.produce(fwd(Var(s)))
   }
 
   override def produce(value: ReadChannel[U]): Unit = {
-    if (!ignore) sub := bwd(value)
+    sub := bwd(value)
     super.produce(value)
   }
 
