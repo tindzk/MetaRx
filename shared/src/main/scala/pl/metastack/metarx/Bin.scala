e63423369f3a25937ffb9fd6cb26e50bdb1a77b5
Tim Nieradzik <tim@metastack.pl>
2016-01-15 11:59:19 +0100

    Bin: Use LazyVar

--- shared/src/main/scala/pl/metastack/metarx/Bin.scala
+++ shared/src/main/scala/pl/metastack/metarx/Bin.scala
@@ -2,18 +2,21 @@ package pl.metastack.metarx
 
 import java.util.concurrent.atomic.AtomicReference
 
-/** Binary channel; use to communicate between two components. Values produced
-  * on `Bin` will be propagated to `left` and `right`, but not between those two.
-  * It can be used for two-way binding.
+/**
+  * Binary channel
+  *
+  * Use to communicate between two components. Values produced on `Bin` will be
+  * propagated to `left` and `right`, but not between those two. It can be used
+  * for two-way binding.
   */
-class Bin[T](init: T)
+class Bin[T](value: T)
   extends StateChannel[T]
   with ChannelDefaultSize[T]
 {
-  private val v = new AtomicReference(init)
+  private val v = new AtomicReference(value)
 
-  val left  = StateChannel[T](v.get)
-  val right = StateChannel[T](v.get)
+  val left  = LazyVar[T](v.get)
+  val right = LazyVar[T](v.get)
 
   left.attach(v.set)
   right.attach(v.set)
@@ -27,9 +30,9 @@ class Bin[T](init: T)
 
   def get: T = v.get
 
-  override def toString = s"Bin(${v.toString})"
+  override def toString = s"Bin($get)"
 }
 
 object Bin {
-  def apply[T](v: T) = new Bin(v)
+  def apply[T](value: T) = new Bin(value)
 }
