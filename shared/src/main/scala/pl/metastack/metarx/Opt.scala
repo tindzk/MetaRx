4dc07259c7cebedad8016b57694ca4034d494318
Tim Nieradzik <tim@metastack.pl>
2016-01-15 12:41:42 +0100

    Opt: Reorder methods

--- shared/src/main/scala/pl/metastack/metarx/Opt.scala
+++ shared/src/main/scala/pl/metastack/metarx/Opt.scala
@@ -6,15 +6,16 @@ object Opt {
   @inline def apply[T](): Opt[T] = Var(Option.empty[T])
   @inline def apply[T](value: T): Opt[T] = Var(Some(value))
 
-  def fromOption[T](future: Future[Option[T]])(implicit exec: ExecutionContext): Opt[T] = {
-    val opt = Opt[T]()
-    future.foreach(v => opt.produce(v))
-    opt
-  }
-
   def from[T](future: Future[T])(implicit exec: ExecutionContext): Opt[T] = {
     val opt = Opt[T]()
     future.foreach(v => opt.produce(Some(v)))
+    opt
+  }
+
+  def fromOption[T](future: Future[Option[T]])
+                   (implicit exec: ExecutionContext): Opt[T] = {
+    val opt = Opt[T]()
+    future.foreach(opt.produce)
     opt
   }
 }
