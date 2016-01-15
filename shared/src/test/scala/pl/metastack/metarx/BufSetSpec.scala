78df1a71cec9218e0957678717064e01cb1d136b
Tim Nieradzik <tim@metastack.pl>
2016-01-15 12:01:49 +0100

    BufSetSpec: Eliminate warnings

--- shared/src/test/scala/pl/metastack/metarx/BufSetSpec.scala
+++ shared/src/test/scala/pl/metastack/metarx/BufSetSpec.scala
@@ -1,15 +1,17 @@
 package pl.metastack.metarx
 
-class BufSetSpec extends CompatTest {
+import org.scalatest.FunSuite
+
+class BufSetSpec extends FunSuite {
   test("Creation") {
     val set = BufSet(1, 1, 1)
-    assertEquals(set.toSet$, Set(1))
+    assert(set.get == Set(1))
   }
 
   test("Inserting") {
     val set = BufSet(42)
     set.insertIfNotExists(23)
-    assertEquals(set.toSet$, Set(42, 23))
+    assert(set.get == Set(42, 23))
     set.insertIfNotExists(23)
     intercept[AssertionError] {
       set.insert(23)
@@ -19,7 +21,7 @@ class BufSetSpec extends CompatTest {
   test("Removing") {
     val set = BufSet(42)
     set.removeIfExists(42)
-    assertEquals(set.toSet$, Set.empty)
+    assert(set.get == Set.empty)
     set.removeIfExists(42)
     intercept[AssertionError] {
       set.remove(42)
@@ -29,12 +31,12 @@ class BufSetSpec extends CompatTest {
   test("Replace") {
     val set = BufSet(1, 2, 3)
     set.replace(2, 4)
-    assertEquals(set.toSet$, Set(1, 3, 4))
+    assert(set.get == Set(1, 3, 4))
   }
 
   test("Update") {
     val set = BufSet(1, 2, 3)
     set.update(_ + 1)
-    assertEquals(set.toSet$, Set(2, 3, 4))
+    assert(set.get == Set(2, 3, 4))
   }
 }
